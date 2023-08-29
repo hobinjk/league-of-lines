897624789f1dfb5be8b0ca4656938eab2f7b3698
James Hobin <hobinjk@gmail.com>
2023-08-29 17:26:15 -0400

    Add game mode selection

--- src/league-of-lines.js
+++ src/league-of-lines.js
@@ -1,11 +1,35 @@
 import './style.css';
 
-// import { GameTimeTrial } from './GameTimeTrial.js';
+import { GameTimeTrial } from './GameTimeTrial.js';
 import { GameMulti } from './GameMulti.js';
 
+function startTimeTrial() {
+  let gameTimeTrial = new GameTimeTrial();
+  gameTimeTrial.onLoad();
+  document.body.removeChild(buttonContainer);
+}
 
-// let gameTimeTrial = new GameTimeTrial();
-// gameTimeTrial.onLoad();
+function startMulti() {
+  new GameMulti();
+  document.body.removeChild(buttonContainer);
+}
 
-let gameMulti = new GameMulti();
-// gameMulti.onLoad();
+let buttonContainer = document.createElement('div');
+buttonContainer.classList.add('button-container');
+
+const trialButton = document.createElement('div');
+trialButton.classList.add('game-button');
+trialButton.textContent = 'Time Trial';
+trialButton.addEventListener('click', startTimeTrial);
+trialButton.addEventListener('touchstart', startTimeTrial);
+buttonContainer.appendChild(trialButton);
+
+
+const multiButton = document.createElement('div');
+multiButton.classList.add('game-button');
+multiButton.textContent = 'Multiplayer';
+multiButton.addEventListener('click', startMulti);
+multiButton.addEventListener('touchstart', startMulti);
+buttonContainer.appendChild(multiButton);
+
+document.body.appendChild(buttonContainer);
